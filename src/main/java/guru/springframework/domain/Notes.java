a99bcbaf9e5ea1c20a516db70e1dc75be8c90d7b
Phuong Nguyen <phuongsover2@gmail.com>
2022-11-12 11:44:04 +0700

    Using Project Lombok.

--- src/main/java/guru/springframework/domain/Notes.java
+++ src/main/java/guru/springframework/domain/Notes.java
@@ -7,6 +7,10 @@ import javax.persistence.Id;
 import javax.persistence.Lob;
 import javax.persistence.OneToOne;
 
+import lombok.Data;
+import lombok.NonNull;
+
+@Data
 @Entity
 public class Notes {
   @Id
@@ -17,37 +21,7 @@ public class Notes {
   private Recipe recipe;
 
   @Lob
+  @NonNull
   private String recipeNotes;
-
-  public Notes(String recipeNotes) {
-    this.recipeNotes = recipeNotes;
-  }
-
-  public Notes() {
-  }
-
-  public Long getId() {
-    return id;
-  }
-
-  public void setId(Long id) {
-    this.id = id;
-  }
-
-  public Recipe getRecipe() {
-    return recipe;
-  }
-
-  public void setRecipe(Recipe recipe) {
-    this.recipe = recipe;
-  }
-
-  public String getRecipeNotes() {
-    return recipeNotes;
-  }
-
-  public void setRecipeNotes(String recipeNotes) {
-    this.recipeNotes = recipeNotes;
-  }
 
 }
